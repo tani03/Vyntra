1cc47bdd0944ecbd4809be5553933daf9a634589
LENOVO <LENOVO@LAPTOP-KG3NP5E1>
2020-05-30 02:43:39 +0530

    -

---
+++ onlinemart/src/main/java/net/vyntra/onlinemart/controller/PageController.java
@@ -0,0 +1,17 @@
+package net.vyntra.onlinemart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView page()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		
+		return mv;
+	}
+}
